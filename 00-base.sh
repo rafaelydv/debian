6219f4109e1616d6bc02e07cb51a87053adfe379
Rafael Y. De Vidis <rafael.ydv@gmail.com>
2020-06-20 14:46:41 -0400

    Pacotes do Networmanager atualizados.

--- 00-base.sh
+++ 00-base.sh
@@ -4,10 +4,10 @@ apt install htop \
 	    vim \
 	    apt-transport-https \
 	    wget \
-	    screenfetch
-# Uncoment for Network Manager install
-# 	    gir1.2-nma-1.0 \
-# 	    libnma-dev \
-# 	    libnma0 \
-#	    network-manager \
-# 	    network-manager-gnome
+	    screenfetch \
+# Uncoment for not install Network Manager
+ 	    gir1.2-nma-1.0 \
+ 	    libnma-dev \
+ 	    libnma0 \
+	    network-manager \
+ 	    network-manager-gnome

eea97027e00eb1d8934ddd51a11a7a6ed3f06f82
Rafael Y. De Vidis <rafael.ydv@gmail.com>
2020-08-31 12:53:37 -0400

    Ajustado identação do script.

--- gnome_install/01-gnome-base.sh
+++ gnome_install/01-gnome-base.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # Gnome custom install
-apt install --no-install-recommends --no-install-suggests gdm3 gnome-core gnome-shell gnome-shell-extensions
+apt install --no-install-recommends --no-install-suggests gdm3 \
+							  gnome-core \
+							  gnome-shell \
+							  gnome-shell-extensions

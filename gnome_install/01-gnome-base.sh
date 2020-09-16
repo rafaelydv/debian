cb40d7150b3d8ac00a7fc9ae737004b5251e22e5
Rafael Y. De Vidis <rafael.ydv@gmail.com>
2020-09-16 12:15:22 -0400

    Ajustado instalação dos pacotes da base do gnome.

--- gnome_install/01-gnome-base.sh
+++ gnome_install/01-gnome-base.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Gnome custom install
-apt install --no-install-recommends --no-install-suggests gdm3 \
-							  gnome-core \
-							  gnome-shell \
-							  gnome-shell-extensions
+echo "GDM install!"
+apt install --no-install-recommends --no-install-suggests gdm3 -y
+echo
+echo "Gnome core install!"
+apt install --no-install-recommends --no-install-suggests gnome-core -y
+echo
+echo "Gnome shell install!"
+apt install --no-install-recommends --no-install-suggests gnome-shell -y
+echo
+echo "Gnome shell extensions install!"
+apt install --no-install-recommends --no-install-suggests gnome-shell-extensions -y

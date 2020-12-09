4f3fd604edf2df40ffa0a8dbf9449d6554f155a8
Rafael Y. De Vidis <rafael.ydv@gmail.com>
2020-12-09 04:00:18 -0400

    Incluído alias de sudo para root.

--- xfce_install/99-configuration.sh
+++ xfce_install/99-configuration.sh
@@ -2,6 +2,8 @@
 # Configuração do bashrc do root
 # Update alias
 echo 'alias update="apt update && apt full-upgrade -y && apt autoremove -y && apt clean -y && apt autoclean -y"' >> /root/.bashrc
+# SU alias
+echo 'alias su="sudo su -"' >> /root/.bashrc
 # Clean history alias for root
 echo 'alias sair="cat /dev/null > ~/.bash_history && history -c && exit"' >> /root/.bashrc
 # Clean history alias for user ryd

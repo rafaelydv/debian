8dff8c4824b9792e67bc645281fee1acdee5eda2
Rafael Y. De Vidis <rafael.ydv@gmail.com>
2020-12-14 17:32:24 -0400

    Criando pasta kde.

---
+++ kde_install/00-base.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+apt install htop \
+	    	vim \
+	    	apt-transport-https \
+	    	wget \
+	    	screenfetch \
+		apt-listbugs \
+	    	network-manager \
+ 	    	network-manager-gnome

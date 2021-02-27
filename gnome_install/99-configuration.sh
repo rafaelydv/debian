29f75bf9b621a61eaa6392e030a276bc716c225d
rafaelydv <rafael.ydv@gmail.com>
2021-02-27 04:01:07 -0400

    Ajustado alias de update.

--- gnome_install/99-configuration.sh
+++ gnome_install/99-configuration.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Configuração do bashrc do root
 # Update alias
-echo 'alias update="apt-get update && apt-get upgrade && apt-get dist-upgrade && apt-get autoremove && apt-get autoclean && apt-get clean"' >> /root/.bashrc
+echo 'alias update="apt update && apt upgrade"' >> /root/.bashrc
 # Clean history alias for root
 echo 'alias sair="cat /dev/null > ~/.bash_history && history -c && exit"' >> /root/.bashrc
 # Clean history alias for user ryd

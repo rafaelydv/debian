b3215e92a6075e1a2755c5a084a5e7be00553387
rafaelydv <56797088+rafaelydv@users.noreply.github.com>
2021-06-20 17:50:51 -0400

    Delete gnome_install directory

--- gnome_install/02-temas.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-apt install arc-theme \
-	    adapta-gtk-theme \
-	    deepin-icon-theme \
-	    numix-icon-theme-circle \
-	    breeze-cursor-theme \
-	    moblin-cursor-theme \
-	    dmz-cursor-theme

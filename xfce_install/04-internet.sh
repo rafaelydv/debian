afd87303cb1cb377c1cd885a588493021485b0ad
Rafael Yoshioka De Vidis <rafael.ydv@gmail.com>
2023-10-30 01:27:59 -0400

    Adicionado pacote do thunderbird.

--- xfce_install/04-internet.sh
+++ xfce_install/04-internet.sh
@@ -4,6 +4,8 @@ apt install firefox-esr \
 	    firefox-esr-l10n-pt-br \
 	    transmission \
 	    transmission-gtk \
+	    thunderbird \
+	    thunderbird-l10n-pt-br \
 	    filezilla \
 	    net-tools \
 	    traceroute \

204bad17dd1af7066ee0bbee513c74eabfa32d3a
Rafael Yoshioka De Vidis <rafael.ydv@gmail.com>
2023-11-06 02:00:57 -0400

    Adicionado tema de icones paper.

--- xfce_install/03-temas.sh
+++ xfce_install/03-temas.sh
@@ -2,6 +2,8 @@
 #
 apt install arc-theme \
 	    deepin-icon-theme \
+	    numix-icon-theme \
 	    numix-icon-theme-circle \
+	    paper-icon-theme \
 	    breeze-cursor-theme \
 	    dmz-cursor-theme
